02e8dfa0cf712beccebd085b270037d527298604
BootCoder <bootcoder@gmail.com>
2015-08-02 12:42:34 -0700

    precomp - change production.rb precomp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,11 +26,13 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # Do not fallback to assets pipeline if a precompiled asset is missed. -- CHANGED
+  config.serve_static_assets = true
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
+
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
